104ea6f2b86543f90fcb617202aac84347d6bf94
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-04-04 05:21:16 +0200

    Cleanup urls.py

--- src/propylon_document_manager/site/urls.py
+++ src/propylon_document_manager/site/urls.py
@@ -1,9 +1,6 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views import defaults as default_views
-from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 
 # API URLS

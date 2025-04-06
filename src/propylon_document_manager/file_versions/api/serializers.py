6d859711f9671679a3c6c8e7395aa0c256507322
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-04-06 19:49:26 +0200

    Refactor serializer for FileVersion and add FileSerializer and custom FileUploadSerializer

--- src/propylon_document_manager/file_versions/api/serializers.py
+++ src/propylon_document_manager/file_versions/api/serializers.py
@@ -1,6 +1,35 @@
 from rest_framework import serializers
 
-from ..models import FileVersion
+from ..models import FileVersion, File
+
+
+class FileUploadSerializer(serializers.Serializer):
+    file_name = serializers.CharField(max_length=512)
+    file_path = serializers.CharField(max_length=512)
+    file = serializers.FileField()
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+
+        file = File.objects.create(
+            file_name=validated_data['file_name'],
+            file_path=validated_data['file_path'],
+            uploaded_by=user
+        )
+
+        file_version = FileVersion.objects.create(
+            file=file,
+            file_content=validated_data['file'],
+        )
+
+        return file_version
+
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = "__all__"
+
 
 class FileVersionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,4 +37,5 @@ class FileVersionSerializer(serializers.ModelSerializer):
         fields = "__all__"
         extra_kwargs = {
             "uploaded_by": {"required": False},
+            "file": {"required": True},
         }

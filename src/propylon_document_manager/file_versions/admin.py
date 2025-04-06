fea98fc97bc240a31d96d1fbb7a6c9df4e218527
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-04-06 20:48:52 +0200

    Register File model to admin panel

--- src/propylon_document_manager/file_versions/admin.py
+++ src/propylon_document_manager/file_versions/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import FileVersion, User
 
+from .models import FileVersion, User, File
+
+admin.site.register(File)
 admin.site.register(FileVersion)
 admin.site.register(User)

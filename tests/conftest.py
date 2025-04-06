2cb8e23a361c6c03d18acf33e5e4aa6c868dddfc
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-04-06 20:39:21 +0200

    Update factories and test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
 import pytest
 
-from propylon_document_manager.file_versions.models import User
-from .factories import UserFactory
+from rest_framework.test import APIClient
+from propylon_document_manager.file_versions.models import User, File, FileVersion
+from .factories import UserFactory, FileFactory, FileVersionFactory
+
 
 @pytest.fixture(autouse=True)
 def enable_db_access_for_all_tests(db):
     pass
+
 
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
@@ -15,3 +18,28 @@ def media_storage(settings, tmpdir):
 @pytest.fixture
 def user(db) -> User:
     return UserFactory()
+
+
+@pytest.fixture
+def users(db) -> list[User]:
+    return UserFactory.create_batch(3)
+
+
+@pytest.fixture
+def file(db, user) -> File:
+    return FileFactory()
+
+
+@pytest.fixture
+def files(db, user) -> list[File]:
+    return FileFactory.create_batch(3, uploaded_by=user)
+
+
+@pytest.fixture
+def file_versions(db, file) -> list[FileVersion]:
+    return FileVersionFactory.create_batch(3, file=file)
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()

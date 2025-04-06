3dbb74488df97880a6f6fe902374d2a66edfa0f7
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-04-06 23:16:13 +0200

    FE: Fetch CSRF Token on App load

--- client/doc-manager/src/App.js
+++ client/doc-manager/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import FileVersions from './FileVersions'
 
+import React, {useEffect} from 'react';
 
 function App() {
   return (
@@ -10,6 +11,17 @@ function App() {
       </header>
     </div>
   );
+    // Fetch the CSRF token when the app loads
+    useEffect(() => {
+        const fetchCsrfToken = async () => {
+            let response = await fetch("http://127.0.0.1:8001/api-auth/login/", {
+                method: "GET",
+                credentials: "include", // Include cookies
+            });
+        };
+
+        fetchCsrfToken();
+    }, []);
 }
 
 export default App;
